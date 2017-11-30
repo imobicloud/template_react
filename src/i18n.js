02fca7e1bc584d17ecc4eaef8fa8b3a6bee32fca
cong <congdv.dhsp@gmai.com>
2017-11-30 14:30:10 +0700

    add folder locales

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,4 @@
 import LocalizedStrings from 'react-localization' // https://github.com/stefalda/react-localization
+import en from './locales/en'
 
-export default new LocalizedStrings({
-    en: {
-        Header: {
-            title: 'Header'
-        },
-        Home: {
-            title: 'Home'
-        },
-        Proposal: {
-            View: {
-                title: 'Proposal View'
-            }
-        },
-        Proposals: {
-            title: 'Proposals'
-        }
-    }
-})
+export default new LocalizedStrings({ en })
---
+++ src/locales/en.js
@@ -0,0 +1,18 @@
+const en =  {
+    Header: {
+        title: 'Header'
+    },
+    Home: {
+        title: 'Home'
+    },
+    Proposal: {
+        View: {
+            title: 'Proposal View'
+        }
+    },
+    Proposals: {
+        title: 'Proposals'
+    }
+}
+
+export default en
